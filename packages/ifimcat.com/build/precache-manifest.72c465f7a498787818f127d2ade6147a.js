8aa5fbfb06fb05bc144d99bed9f9507ffc766949
MoGoethe <1095364870@qq.com>
2020-08-21 09:20:45 +0800

    update

---
+++ packages/ifimcat.com/build/precache-manifest.72c465f7a498787818f127d2ade6147a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "93cfda140ddace2c3a9844df454ba1e0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0ebdbb362e0e03bf8bb1",
+    "url": "/static/css/main.b6eecf7f.chunk.css"
+  },
+  {
+    "revision": "4638bd41c48d2f7988fa",
+    "url": "/static/js/2.bedb25c4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.bedb25c4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0ebdbb362e0e03bf8bb1",
+    "url": "/static/js/main.e2555435.chunk.js"
+  },
+  {
+    "revision": "403103756fff9205c9e9",
+    "url": "/static/js/runtime-main.96a9b040.js"
+  }
+]);
--- packages/ifimcat.com/src/App.js
+++ packages/ifimcat.com/src/App.js
@@ -1,12 +1,11 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import "./index.scss"
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src="http://tjholowaychuk.imgix.net/image/collections/Europe/main.jpg" className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
